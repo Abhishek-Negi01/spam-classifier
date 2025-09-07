520fe449c735efabfaa05ae346dd0c65e64b6227
Abhishek Negi <negiabhishek75054@gmail.com>
2025-09-07 22:56:06 +0530

    some deployement related issue

--- spam_project/spam_project.py
+++ spam_project/spam_project.py
@@ -5,15 +5,19 @@ import string
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
+
+
 try:
     nltk.data.find('tokenizers/punkt')
 except LookupError:
     nltk.download('punkt')
 
+
 try:
     nltk.data.find('corpora/stopwords')
 except LookupError:
     nltk.download('stopwords')
+
 
 ps = PorterStemmer()
 
